2ac513a009016a1ff9a13f90f5d76abc21f4768b
Heer-11 <84759793+Heer-11@users.noreply.github.com>
2021-05-26 22:32:26 +0530

    v1

---
+++ day2/marksheet.php
@@ -0,0 +1,38 @@
+<?php
+
+$name = "shree";
+$regno = 1;
+
+$s1 = 40;
+$s2 = 20;
+$s3 = 30;
+
+$t = $s1 + $s2 + $s3;
+$p = $t / 3;
+
+if ($p >= 60) {
+    echo " first division";
+}
+if ($p >= 50 and $p < 60) {
+    echo " Second division";
+}
+if ($p >= 40 and $p < 50) {
+    echo " Third division";
+}
+
+if ($p < 40) {
+    echo " fail";
+}
+
+
+
+echo "<br>";
+if ($s1 >= 35 and $s2 >= 35 and $s3 >= 35) {
+    $r = "pass";
+} else {
+    $r = "fail";
+}
+
+echo $r;
+echo $t;
+echo $p;
---
+++ day2/print.php
@@ -0,0 +1,15 @@
+<?php
+//variable
+$a = 10;
+$b = 20;
+//Method1
+echo "A value is " . $a;
+echo "<br/>B Value is" . $b;
+//Method2
+echo "<br/>A Value is" . $a . "<br/>B value is " . $b;
+//Method3
+echo "<br/>A Value is $a<br/>B value is $b";
+//Mathod4
+echo '<br/>A Value is $a<br/>B Value is $b';
+//Method5
+echo "<br/>A Value is $a<br/>B Value is $b Sum is" . ($a + $b);
